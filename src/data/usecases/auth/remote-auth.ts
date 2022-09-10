741af84e92d03af39e239fac7298f9c5c8906157
Janku13 <mohammed.mmaljoke@gmail.com>
2022-09-10 18:47:16 -0300

    feat: ensure remote auth throw on error

--- src/data/usecases/auth/remote-auth.ts
+++ src/data/usecases/auth/remote-auth.ts
@@ -1,5 +1,7 @@
 import { AuthParams } from '@/domain/usecases/auth'
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 
 
 export class RemoteAuthentication{
@@ -9,9 +11,13 @@ export class RemoteAuthentication{
   ) {}
   
   async auth(params:AuthParams): Promise<void> {
-    await this.httpPostClient.post({
+    const httpResponse =  await this.httpPostClient.post({
       url: this.url,
       body:params
     })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
+      default: return Promise.resolve()
+    }
   }
 }
