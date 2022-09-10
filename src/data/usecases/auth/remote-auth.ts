cdf39aba825b12b3e68e03839651af7b1a56b3fd
Janku13 <mohammed.mmaljoke@gmail.com>
2022-09-10 19:02:04 -0300

    feat: ensure remoteAuth throws erros on bad request

--- src/data/usecases/auth/remote-auth.ts
+++ src/data/usecases/auth/remote-auth.ts
@@ -2,6 +2,7 @@ import { AuthParams } from '@/domain/usecases/auth'
 import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
+import { UnexpectedError } from '@/domain/errors/unexpected-error'
 
 
 export class RemoteAuthentication{
@@ -16,8 +17,10 @@ export class RemoteAuthentication{
       body:params
     })
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:break
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
-      default: return Promise.resolve()
+      case HttpStatusCode.badRequest: throw new UnexpectedError()
+      default: throw new UnexpectedError()
     }
   }
 }
